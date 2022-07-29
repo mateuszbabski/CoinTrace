4e8895bb31113d43dcc6b83b05c8bbd6b0a319ac
mateuszbabski <babskimateusz@gmail.com>
2022-07-29 14:38:17 +0200

    updated user entity

--- Domain/Entities/User.cs
+++ Domain/Entities/User.cs
@@ -13,7 +13,8 @@ namespace Domain.Entities
         public string? PhoneNumber { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-        public string PasswordHash { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
         public string Role { get; set; }
     }
 }
